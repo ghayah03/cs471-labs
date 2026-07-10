bfabdf4fe4e5bbe10857f862d63778c806a4ed1a
Ghayah <ghayah233@gmail.com>
2026-07-10 22:01:09 +0300

    Complete Lab 4

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -61,3 +61,17 @@ def book_detail(request, book_id):
     return render(request, "bookmodule/detail.html", context)
 
 
+def index(request):
+    return render(request, "bookmodule/index.html")
+
+
+def list_books(request):
+    return render(request, "bookmodule/list_books.html")
+
+
+def viewbook(request, bookId):
+    return render(request, "bookmodule/one_book.html")
+
+
+def aboutus(request):
+    return render(request, "bookmodule/aboutus.html")
