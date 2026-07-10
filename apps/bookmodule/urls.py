68df0425bbc8197f43cfcd28884a165547020596
Ghayah <ghayah233@gmail.com>
2026-07-10 19:32:07 +0300

    Complete Lab 3

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('index2/<int:val1>/', views.index2),
+    path('books/<int:book_id>/', views.book_detail),
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,4 +1,63 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Welcome to Book Module")
+    books = [
+        {
+            "title": "Python Programming",
+            "author": "John Smith",
+            "year": 2020
+        },
+        {
+            "title": "Django Web Development",
+            "author": "David Brown",
+            "year": 2022
+        },
+        {
+            "title": "Web Technologies",
+            "author": "Mark Lee",
+            "year": 2023
+        }
+    ]
+
+    context = {
+        "books": books
+    }
+
+    return render(request, "bookmodule/index.html", context)
+
+
+
+def index2(request, val1=0):
+    return HttpResponse("value1 = " + str(val1))
+
+def book_detail(request, book_id):
+    books = [
+        {
+            "id": 1,
+            "title": "Python Programming",
+            "author": "John Smith",
+            "year": 2020
+        },
+        {
+            "id": 2,
+            "title": "Django Web Development",
+            "author": "David Brown",
+            "year": 2022
+        }
+    ]
+
+    book = None
+
+    for b in books:
+        if b["id"] == book_id:
+            book = b
+
+    context = {
+        "book": book
+    }
+
+    return render(request, "bookmodule/detail.html", context)
+
+
